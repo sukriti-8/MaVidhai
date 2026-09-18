99d783225633371569d59387ae8d1d5b7f4a99eb
http-samee <sameeksha0307@gmail.com>
2026-09-18 21:27:55 +0500

    Add application color theme

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    ivory: "#F8F6F2",
+    white: "#FFFFFF",
+    peach: "#F2C9B9",
+    terracotta: "#A85838",
+    sage: "#A8B39F",
+    forest: "#3F5144",
+    charcoal: "#1D1D1B",
+  },
+};
+
+export default theme;
